aa3de3270ed5f26017388948c6f610528ffea463
alvaropineros.dev <alvaro.pineros.dev@gmail.com>
2026-04-30 17:31:37 -0500

    Refactored Db class: added static methods for fetch_all and execute_write with proper structure

--- db.py
+++ db.py
@@ -1,67 +1,57 @@
-#Db.py
-
 import mysql.connector
 from mysql.connector import Error
 
-class Db: 
-    def get_connection():
+class Db:
 
+    @staticmethod
+    def get_connection():
         try:
             connection = mysql.connector.connect(
-                    host = "localhost",
-            user = "root",
-                password = "root",
-                database = "proyecto_1"
+                host="localhost",
+                user="root",
+                password="root",
+                database="proyecto_1"
             )
-            return connection #Se retornan los datos para poder usar la conexion fuera de la funcion.
-    
+            return connection
         except Error as tipo_error:
             print(f"Error de conexion: {tipo_error}")
-            return None #Retornamos none para indicar un error, es una forma standar de python
-    
-    if __name__ == "__main__":
-        connection = get_connection()
-
-        if connection:
-            print("Conexion exitosa")
-            connection.close()
-        else: 
-            print ("Fallo la conexion")
-    
-
-        #Hacer la consulta bompleta
-def fetch_all(query, params=None):
-
-    conexion = get_connection()
-    cursor = conexion.cursor()
-
-    if params is None:
-        cursor.execute(query)
-    else:
-        cursor.execute(query, params)
-
-    dato = cursor.fetchall()
-    cursor.close()
-    conexion.close()
-
-    return dato
+            return None
 
 
-def execute_write(query, params=None):
+    @staticmethod
+    def fetch_all(query, params=None):
 
-    conexion = get_connection()
-    cursor = conexion.cursor()
+        conexion = Db.get_connection()
+        cursor = conexion.cursor()
 
-    if params is None:
-        cursor.execute(query)
-    else:
-        cursor.execute(query, params)
+        if params is None:
+            cursor.execute(query)
+        else:
+            cursor.execute(query, params)
 
-    conexion.commit()
-    affected = cursor.rowcount
-    cursor.close()
-    conexion.close()
+        dato = cursor.fetchall()
+        cursor.close()
+        conexion.close()
 
-    return affected
-    
+        return dato
+
+
+    @staticmethod
+    def execute_write(query, params=None):
+
+        conexion = Db.get_connection()
+        cursor = conexion.cursor()
+
+        if params is None:
+            cursor.execute(query)
+        else:
+            cursor.execute(query, params)
+
+        conexion.commit()
+        affected = cursor.rowcount
+
+        cursor.close()
+        conexion.close()
+
+        return affected
 
